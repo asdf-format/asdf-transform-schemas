e54ff67b18308282f092fafbf9b8f54574798c5d
William Jamieson <wjamieson@stsci.edu>
2022-01-07 17:04:49 -0500

    Made all invalid tests work off schema in manifest.

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -5,125 +5,184 @@ from asdf.tests import helpers
 
 purposefully_failing = [
     # bounding_box fails
-    """!transform/bounding_box-1.0.0
-  [1.0, test]
+    # 1D string bound
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box: [1.0, test]
     """,
-    """!transform/bounding_box-1.0.0
-  [1.0]
+    # 1D only one bound
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box: [1.0]
     """,
-    """!transform/bounding_box-1.0.0
-  [1.0, 2.0, 3.0]
+    # 1D more than two bounds
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box: [1.0, 2.0, 3.0]
     """,
-    """!transform/bounding_box-1.0.0
-  - [1.0, 2.0]
+    # 2D only one interval
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box:
+    - [1.0, 2.0]
     """,
-    """!transform/bounding_box-1.0.0
-  - [1.0, 2.0]
-  - [1.0, 2.0, 3.0]
+    # 2D string bound
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box:
+    - [1.0, 2.0]
+    - [3.0, test]
     """,
-    """!transform/bounding_box-1.0.0
-  - [1.0, 2.0]
-  - [1.0, test]
+    # 2D only one bound
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box:
+    - [1.0, 2.0]
+    - [3.0]
     """,
-    """!transform/bounding_box-1.0.0
-  - [1.0]
-  - [1.0, 2.0]
+    # 2D more than two bounds
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box:
+    - [1.0, 2.0]
+    - [3.0, 4.0, 5.0]
     """,
     # compound_bounding_box fails
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # missing ignored in selector
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - ignored: true
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # missing argument in selector
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - ignored: true
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: 5
-    - ignored: true
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # argument is not a string
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: 5
+        ignored: true
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: test
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # ignored is not a boolean
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: test
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args: []
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # Nothing in selector_args array
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args: []
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox:
-    - key: []
-      bbox: [1.0, 2.0]
+    # Missing selector_args array
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox:
-    - key: [test]
-      bbox: [1.0, 2.0]
+    # Nothing in key array
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox:
+      - key: []
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox:
-    - bbox: [1.0, 2.0]
+    # Non-numeric key entry
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox:
+      - key: [test]
+        bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0, 3.0]
+    # Missing key entry
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox:
+      - bbox: [1.0, 2.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox:
-    - key: [0]
+    # Invalid bbox entry
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox:
+      - key: [0]
+        bbox: [1.0, 2.0, 3.0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
-  cbbox: []
+    # Missing bbox entry
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox:
+      - key: [0]
     """,
-    """!transform/compound_bounding_box-1.0.0
-  cbbox:
-    - key: [0]
-      bbox: [1.0, 2.0]
+    # Empty cbbox array
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
+    cbbox: []
     """,
-    """!transform/compound_bounding_box-1.0.0
-  selector_args:
-    - argument: x
-      ignored: true
+    # Missing cbbox entry
+    """!transform/shift-1.2.0
+  offset: 1
+  compound_bounding_box:
+    selector_args:
+      - argument: x
+        ignored: true
     """,
     # transform fails (both bounding_box and compound_bounding_box)
-    """!transform/transform-1.2.0
-  name: test
-  bounding_box: [1.0, 2.0]
+    """!transform/shift-1.2.0
+  offset: 1
+  bounding_box:
+    - [1.0, 2.0]
+    - [3.0, 4.0]
   compound_bounding_box:
     selector_args:
       - argument: x
