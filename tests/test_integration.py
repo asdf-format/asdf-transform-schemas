8280ace60ef7c56f954f0f1f6f32e1378e7dc8b4
William Jamieson <wjamieson@stsci.edu>
2022-02-11 15:32:06 -0500

    Refactor `test_manifests`

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,11 +7,12 @@ import yaml
 
 def get_resources():
     resources_root = Path(__file__).parent.parent / "resources"
+
     return list(resources_root.glob("**/*.yaml"))
 
 
 @pytest.mark.parametrize("resource_path", get_resources())
-def test_resources(resource_path):
+def test_resource(resource_path):
     resource_manager = asdf.get_config().resource_manager
 
     with resource_path.open("rb") as f:
@@ -21,24 +22,29 @@ def test_resources(resource_path):
     assert resource_manager[resource_uri] == resource_content
 
 
-def test_manifests():
+def get_manifests():
     manifests_root = (
         Path(__file__).parent.parent / "resources" / "asdf-format.org" / "manifests"
     )
+
+    return list(manifests_root.glob("*.yaml"))
+
+
+@pytest.mark.parametrize("manifest_path", get_manifests())
+def test_manifest(manifest_path):
     resource_manager = asdf.get_config().resource_manager
 
-    for manifest_path in manifests_root.glob("*.yaml"):
-        with manifest_path.open("rb") as f:
-            manifest_content = f.read()
-        manifest = yaml.safe_load(manifest_content)
+    with manifest_path.open("rb") as f:
+        manifest_content = f.read()
+    manifest = yaml.safe_load(manifest_content)
 
-        manifest_schema = asdf.schema.load_schema(
-            "asdf://asdf-format.org/core/schemas/extension_manifest-1.0.0"
-        )
+    manifest_schema = asdf.schema.load_schema(
+        "asdf://asdf-format.org/core/schemas/extension_manifest-1.0.0"
+    )
 
-        # The manifest must be valid against its own schema:
-        asdf.schema.validate(manifest, schema=manifest_schema)
+    # The manifest must be valid against its own schema:
+    asdf.schema.validate(manifest, schema=manifest_schema)
 
-        for tag_definition in manifest["tags"]:
-            # The tag's schema must be available:
-            assert tag_definition["schema_uri"] in resource_manager
+    for tag_definition in manifest["tags"]:
+        # The tag's schema must be available:
+        assert tag_definition["schema_uri"] in resource_manager
