2308a5bf80812e328016e64d59c44b779a718ee5
William Jamieson <wjamieson@stsci.edu>
2022-02-24 15:06:49 -0500

    Merge pull request #32 from WilliamJamieson/feature/recursive_package_data
    Add recursive yaml resource finding.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from pathlib import Path
 from setuptools import setup, find_packages
 
 
@@ -10,9 +11,13 @@ package_dir = {
     "asdf_transform_schemas.resources": "resources",
 }
 
-package_data = {
-    "asdf_transform_schemas.resources": ["*.yaml", "**/*.yaml", "**/**/*.yaml"],
-}
+
+def package_yaml_files(directory):
+    paths = sorted(Path(directory).rglob("*.yaml"))
+    return [str(p.relative_to(directory)) for p in paths]
+
+
+package_data = {"asdf_transform_schemas.resources": package_yaml_files("resources")}
 
 setup(
     use_scm_version=True,
